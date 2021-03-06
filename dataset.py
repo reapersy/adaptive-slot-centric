8e1af7d272d07e5f407dd9eb0b4f5376463e227a
reapersy <reapersoy@proton.me>
2021-03-06 14:46:38 +0000

    i18n: [python] Automatic merge of Transifex translations

--- dataset.py
+++ dataset.py
@@ -5,4 +5,9 @@ import glob
 import torchvision
 import numpy as np
 import ipdb
-import torch.nn.functional as 
+import torch.nn.functional as F
+st = ipdb.set_trace
+import torch
+import time
+
+class ClevrDataset(torch.utils.d
