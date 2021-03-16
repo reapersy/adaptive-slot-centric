9b22775168c62e950147ee7028dc7b5d1eef8bcd
reapersy <reapersoy@proton.me>
2021-03-16 10:53:55 +0000

    updated utils

--- dataset.py
+++ dataset.py
@@ -16,4 +16,4 @@ class ClevrDataset(torch.utils.data.Dataset):
         root_file = opt.root_folder
         self.all_files = glob.glob(f'{root_file}/clevr_train/*')
 
-        se
+        self.resize = torchvision.transforms.Resize((opt.image_height,opt.image_width
