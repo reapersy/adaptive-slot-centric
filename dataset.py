c54ab2ca94c89597d5b520c3b8b7ec0ae24fd2ae
reapersy <reapersoy@proton.me>
2021-08-15 06:08:43 +0000

    build h5

--- dataset.py
+++ dataset.py
@@ -55,4 +55,4 @@ class ClevrTexDataset(torch.utils.data.Dataset):
         root_file = opt.root_folder
         self.all_files = glob.glob(f'{root_file}/clevr_tex/*')
         self.resize = torchvision.transforms.Resize((opt.image_height,opt.image_width))
-        self.resize_mask = torchvision
+        self.resize_mask = torchvision.transforms.Resize((opt.image_height,opt.image_width),torchvision.transforms.In
