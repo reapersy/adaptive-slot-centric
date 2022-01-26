7076cfadeec5aef1e67b40d5b588cb51b2d99722
reapersy <reapersoy@proton.me>
2022-01-26 15:56:58 +0000

    sparc32: Implement xchg and atomic_xchg using ATOMIC_HASH locks

--- dataset.py
+++ dataset.py
@@ -121,4 +121,7 @@ def get_data_tta(opt):
     if opt.dataset_name == "clevr_tex":
         dataset = ClevrTexDataset(opt)
     else:
-        dataset =
+        dataset = ClevrDataset(opt)
+    return dataset
+
+def get_input(opt, iterator, train_loader):
