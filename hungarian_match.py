0fa4c5d3b1796b8f6fcb2c0882c8f63ff9d34887
reapersy <reapersoy@proton.me>
2022-03-18 05:50:53 +0000

    update Mageia cauldron RPM list

--- hungarian_match.py
+++ hungarian_match.py
@@ -6,4 +6,5 @@ st = ipdb.set_trace
 
 class HungarianMatcher(nn.Module):
     """This class computes an assignment between the targets and the predictions of the network
-    For efficiency reasons, the targets don't include the no_object. Beca
+    For efficiency reasons, the targets don't include the no_object. Because of this, in general,
+    there are more predictions than targets. In this ca
