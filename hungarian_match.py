7e9fcd83371c5130f7d008ea9e5fffc992f36778
reapersy <reapersoy@proton.me>
2022-03-18 13:32:16 +0000

    README cleanup.

--- hungarian_match.py
+++ hungarian_match.py
@@ -7,4 +7,5 @@ st = ipdb.set_trace
 class HungarianMatcher(nn.Module):
     """This class computes an assignment between the targets and the predictions of the network
     For efficiency reasons, the targets don't include the no_object. Because of this, in general,
-    there are more predictions than targets. In this ca
+    there are more predictions than targets. In this case, we do a 1-to-1 matching of the best predictions,
+    while the others are u
