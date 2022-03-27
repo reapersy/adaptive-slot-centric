db7d1690c1559dc20feac778932ac97820d394f5
reapersy <reapersoy@proton.me>
2022-03-27 17:05:38 +0000

    added info to readme

--- hungarian_match.py
+++ hungarian_match.py
@@ -25,4 +25,7 @@ class HungarianMatcher(nn.Module):
         assert cost_class != 0 or cost_bbox != 0 or cost_giou != 0, "all costs cant be 0"
 
     @torch.no_grad()
-    def forward(self, outputs, targets, do_sigmoid = 
+    def forward(self, outputs, targets, do_sigmoid = False, use_mm=False):
+        """ Performs the matching
+        Params:
+   
