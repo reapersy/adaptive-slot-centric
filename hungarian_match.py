c1bc93068bfc75f2a59e9f942559c3c3010f3b4e
reapersy <reapersoy@proton.me>
2022-04-05 22:08:16 +0000

    - refactoring

--- hungarian_match.py
+++ hungarian_match.py
@@ -31,4 +31,4 @@ class HungarianMatcher(nn.Module):
             outputs: This is a dict that contains at least these entries:
                  "pred_logits": Tensor of dim [batch_size, num_queries, num_classes] with the classification logits
                  "pred_boxes": Tensor of dim [batch_size, num_queries, 4] with the predicted box coordinates
-            tar
+            targets: This is a list of targets (len(targets) = batch_size), where each target 
