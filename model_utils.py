39e7de70841cd167a66fc1ab513fbbb5fb1546e4
reapersy <reapersoy@proton.me>
2022-06-27 08:40:34 +0000

    Updated dev-master in all components

--- model_utils.py
+++ model_utils.py
@@ -5,4 +5,8 @@ import ipdb
 st = ipdb.set_trace
 
 def get_model_and_optimizer(opt):
-    model = m
+    model = models.ModelIter(opt)
+    model = model.to(opt.device)
+    
+    if opt.tta_optimizer == "adam":
+   
