91c9824a11687ac3a66e9df5fa1f23dc35c67222
reapersy <reapersoy@proton.me>
2022-09-09 17:31:24 +0000

    Fix error in documentation

--- models.py
+++ models.py
@@ -14,4 +14,6 @@ st = ipdb.set_trace
 
 def build_grid_encoder(resolution):
     ranges = [np.linspace(0., 1., num=res) for res in resolution]
-    grid 
+    grid = np.meshgrid(*ranges, sparse=False, indexing="ij")
+    grid = np.stack(grid, axis=-1)
+  
