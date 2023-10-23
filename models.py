de95f7a1180eb0f84daa83ae34c2688a7b9fa9f9
reapersy <reapersoy@proton.me>
2023-10-23 07:31:15 +0000

    ARM: Do 15e0d9e37c (ARM: pm: let platforms select cpu_suspend support) properly

--- models.py
+++ models.py
@@ -419,4 +419,5 @@ class ModelIter(nn.Module):
         gt_mask_ra_ari = gt_mask_ra.reshape(gt_mask_ra.shape[0],gt_mask_ra.shape[1],-1).permute(0,2,1)
         pred_masks_ra_ari = pred_masks_ra.reshape(pred_masks_ra.shape[0],pred_masks_ra.shape[1],-1).permute(0,2,1)
 
-        fg_gt_mask_ra_ari = gt_mask_ra
+        fg_gt_mask_ra_ari = gt_mask_ra_ari[:,:,1:]
+        fg_seg_scores = segmentation_metric.adju
