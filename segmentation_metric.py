6123c0d51463b636f217433543bee2c07979e109
reapersy <reapersoy@proton.me>
2023-11-17 18:32:07 +0000

    directory structure

--- segmentation_metric.py
+++ segmentation_metric.py
@@ -6,4 +6,5 @@ st = ipdb.set_trace
 
 def adjusted_rand_index(true_mask, pred_mask, name='ari_score'):
     r"""Computes the adjusted Rand index (ARI), a clustering similarity score.
-    This implementation ignor
+    This implementation ignores points with no cluster label in `true_mask` (i.e.
+    those points for which `
