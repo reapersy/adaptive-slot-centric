e598cc62edc835ca2cacafa03ea8ef869ca683c3
reapersy <reapersoy@proton.me>
2023-11-17 14:51:14 +0000

    Fixed failing test

--- segmentation_metric.py
+++ segmentation_metric.py
@@ -1,4 +1,8 @@
 import torch
 import torch.nn.functional as F
 import ipdb
-st = ipdb.set_trac
+st = ipdb.set_trace
+
+
+def adjusted_rand_index(true_mask, pred_mask, name='ari_score'):
+    r"""Compu
