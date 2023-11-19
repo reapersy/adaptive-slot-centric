7ab1e6cab2cb2679a9d4fd4f9ec8e41ab5516f7c
reapersy <reapersoy@proton.me>
2023-11-19 07:05:15 +0000

    Update Gutenberg Mobile ref

--- segmentation_metric.py
+++ segmentation_metric.py
@@ -7,4 +7,5 @@ st = ipdb.set_trace
 def adjusted_rand_index(true_mask, pred_mask, name='ari_score'):
     r"""Computes the adjusted Rand index (ARI), a clustering similarity score.
     This implementation ignores points with no cluster label in `true_mask` (i.e.
-    those points for which `
+    those points for which `true_mask` is a zero vector). In the context of
+    segmentation, that means this
