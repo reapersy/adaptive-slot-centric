f9789348160a2e38c326696468c1e58c54a04982
reapersy <reapersoy@proton.me>
2023-11-19 18:13:41 +0000

    Archivos

--- segmentation_metric.py
+++ segmentation_metric.py
@@ -8,4 +8,5 @@ def adjusted_rand_index(true_mask, pred_mask, name='ari_score'):
     r"""Computes the adjusted Rand index (ARI), a clustering similarity score.
     This implementation ignores points with no cluster label in `true_mask` (i.e.
     those points for which `true_mask` is a zero vector). In the context of
-    segmentation, that means this
+    segmentation, that means this function can ignore points in an image
+    corresponding to the background (
