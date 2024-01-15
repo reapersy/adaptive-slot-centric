d0144b697b734cc8acbb9b5b30e774bb1754350d
reapersy <reapersoy@proton.me>
2024-01-15 13:17:34 +0000

    add readme.

--- segmentation_metric.py
+++ segmentation_metric.py
@@ -34,4 +34,5 @@ def adjusted_rand_index(true_mask, pred_mask, name='ari_score'):
     """
     _, n_points, n_true_groups = true_mask.shape
     n_pred_groups = pred_mask.shape[-1]
-    if n_points <= n_true_groups and n_poin
+    if n_points <= n_true_groups and n_points <= n_pred_groups:
+      # This rules out the n_true_groups == n
